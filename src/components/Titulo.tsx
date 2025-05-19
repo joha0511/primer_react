fa1fac5061bc1f517b2bef56e8a4a43ca35e7d46
Joha0511 <erikajohanna12.ec@gmail.com>
2025-05-19 19:36:00 -0400

    Version 0002

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    return(
+        <div className="App">
+        <h1>Welcome to React</h1>
+        <p>To get started, edit <code>src/App.tsx</code> and save to reload</p>
+        </div>
+    )
+}
+
+export default Titulo;
